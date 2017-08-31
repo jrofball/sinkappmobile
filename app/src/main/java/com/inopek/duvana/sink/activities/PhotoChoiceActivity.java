d112858ac566fb4ad3b996daca10d8b84d19a666
johanna rojas <johanna.rojas@econocom.com>
2017-08-31 20:21:04 +0200

    Correction grants

--- app/src/main/java/com/inopek/duvana/sink/activities/PhotoChoiceActivity.java
+++ app/src/main/java/com/inopek/duvana/sink/activities/PhotoChoiceActivity.java
@@ -1,6 +1,8 @@
 package com.inopek.duvana.sink.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -18,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import static com.inopek.duvana.sink.constants.SinkConstants.INTENT_EXTRA_FULL_SIZE_FILE_NAME;
 import static com.inopek.duvana.sink.constants.SinkConstants.PHOTO_REQUEST_CODE;
@@ -26,6 +29,7 @@ public class PhotoChoiceActivity extends AppCompatActivity implements OnClickLis
 
     private static final int CAMERA_REQUEST_CODE = 0;
     private static final int SELECT_REQUEST_CODE = 1;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +53,17 @@ public class PhotoChoiceActivity extends AppCompatActivity implements OnClickLis
                 // Launch camera
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 File photoFile = createImageFile();
+                List<ResolveInfo> resInfoList = this.getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
+
                 // Continue only if the File was successfully created
                 if (photoFile != null) {
                     Uri photoURI = FileProvider.getUriForFile(this,
-                            "com.inopek.duvana.sink.fileprovider",
+                            "com.inopek.duvana.sink.provider",
                             photoFile);
+                    for (ResolveInfo resolveInfo : resInfoList) {
+                        String packageName = resolveInfo.activityInfo.packageName;
+                        this.grantUriPermission(packageName, photoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    }
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                     startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
                 }
@@ -71,24 +81,24 @@ public class PhotoChoiceActivity extends AppCompatActivity implements OnClickLis
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (data != null) {
-            Intent intent = new Intent();
-            String fullSizePath = null;
-            if (requestCode == CAMERA_REQUEST_CODE) {
-                File imgFile = new File(mCurrentPhotoPath);
-                if (imgFile.exists()) {
-                    fullSizePath = imgFile.getAbsolutePath();
-                }
-            } else if (requestCode == SELECT_REQUEST_CODE) {
-                fullSizePath = PathUtils.generatePath(data.getData(), getApplicationContext());
+        String fullSizePath = null;
+        if(requestCode == CAMERA_REQUEST_CODE && mCurrentPhotoPath != null) {
+            File imgFile = new File(mCurrentPhotoPath);
+            if (imgFile.exists()) {
+                fullSizePath = imgFile.getAbsolutePath();
             }
+        } else if (data != null && requestCode == SELECT_REQUEST_CODE) {
+          fullSizePath = PathUtils.generatePath(data.getData(), getApplicationContext());
+        }
+        if(fullSizePath != null) {
+            Intent intent = new Intent();
             intent.putExtra(INTENT_EXTRA_FULL_SIZE_FILE_NAME, fullSizePath);
             setResult(PHOTO_REQUEST_CODE, intent);
-            finish();
         }
+        finish();
     }
 
-    String mCurrentPhotoPath;
+
 
     private File createImageFile() {
         // Create an image file name
